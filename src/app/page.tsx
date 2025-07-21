c2cd540fb17b2f2c77bd78791414aac5e066e4bc
JassimKabir <jassimmkabir@gmail.com>
2025-07-21 19:29:57 +0100

    Adds metadata for seo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 import { DotPattern } from '@/components/magicui/dot-pattern';
 import { cn } from '@/lib/utils';
+import { Metadata } from 'next';
+
+const metadata: Metadata = {
+  title: 'Ragooty Sasidharan | Photography',
+  description:
+    'Discover the photography portfolio of Ragooty Sasidharan, specializing in portrait, landscape, and event photography. Explore stunning visual stories, creative projects, and professional photo galleries that capture moments with artistry and passion.',
+};
 
 export default function Home() {
   return (
