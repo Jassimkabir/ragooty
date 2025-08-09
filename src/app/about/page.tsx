f3d7c9b3747e84d6d85eb303935f73b729b99db5
Jassim M Kabir <jassimmkabir@gmail.com>
2025-08-09 14:42:27 +0530

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,15 +3,6 @@ import Image from 'next/image';
 export default function About() {
   return (
     <div>
-      <div></div>
-      <div>
-        <Image
-          src='/images/ragooty-alternative.JPG'
-          alt='Ragooty Sasidharan'
-          width={400}
-          height={300}
-        />
-      </div>
     </div>
   );
 }
